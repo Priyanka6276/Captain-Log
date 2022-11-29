538a83ebf4cdd4b8c9f42f22cbc7ef7fa4823ac6
Priyanka6276 <priyankachanbrasegaran@gmail.com>
2022-11-29 15:27:00 -0500

    2nd commit: did the create route

--- server.js
+++ server.js
@@ -7,13 +7,25 @@ app.set("view engine", "jsx")
 app.engine("jsx", reactViews.createEngine())
 
 app.use((req,res,next) => {
+    console.log("middleware")
     next()
 })
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended:false}))
 
+app.get("/logs", (req,res) => {
+    res.send("hello")
+})
+
+//NEW
 app.get("/new", (req,res) => {
     res.render("New")
+})
+
+//CREATE
+app.post("/logs", (req,res) => {
+    console.log(req.body)
+    res.send(req.body)
 })
 
 app.listen(PORT, () => {
--- views/New.jsx
+++ views/New.jsx
@@ -4,12 +4,15 @@ class New extends React.Component {
     render() {
         return (
             <div>
-                <form action="/logs" method="POST"></form>
-                Title: <input type="text" name="title" /> <br />
-                Entry: <input type="text" /> <br />
-                Ship Is Broken: <input type="checkbox" />
-                <input type="submit" value="Submit" />
+                <form action="/logs" method="POST">
+                    Title: <input type="text" name="title" /> <br />
+                    Entry: <input type="text" name="entry" /> <br />
+                    Ship Is Broken: <input type="checkbox" name="shipIsBroken" value="true"/>
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
         )
     }
 }
+
+module.exports = New
